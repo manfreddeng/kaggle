36cc5710006f9fe67dcfd2c2654c6e1685851c64
Manfred Deng <manfreddeng@gmail.com>
2016-08-15 20:48:32 +1000

    Added function to update each batch

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -8,21 +8,32 @@ class Network(object):
         self.num_layers = len(sizes)
         self.sizes = sizes
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
-        self.weights = [np.random.randn(y, x)
-                        for x, y in zip(sizes[:-1], sizes[1:])]
+        self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
 
 
-    def SGD(self, training_data, epochs, mini_batch_size, eta):
-        if test_data: n_test = len(test_data)
+    def SGD(self, training_data, epochs, mini_batch_size, eta, test_data):
+        n_test = len(test_data)
         n = len(training_data)
         for j in xrange(epochs):
             random.shuffle(training_data)
             mini_batches = [training_data[k:k+mini_batch_size] for k in xrange(0, n, mini_batch_size)]
             for mini_batch in mini_batches:
                 self.update_mini_batch(mini_batch, eta)
-            print "Test {0} complete".format(j)
+            print "Test {0}: {1} / {2}".format(j, self.evaluate(test_data), n_test)
 
     def update_mini_batch(self, mini_batch, eta):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        for x, y in mini_batch:
+            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+        self.weights = [w-(eta/len(mini_batch))*nw for w, nw in zip(self.weights, nabla_w)]
+        self.biases = [b-(eta/len(mini_batch))*nb for b, nb in zip(self.biases, nabla_b)]
+
+    def backprop(self, x, y):
+
+    def evaluate(self, test_data):
 
 def sigmoid(z):
     return 1.0/(1.0+np.exp(-z))
