2d4a54af534afe50ded06cacd548054f2b1ce5f2
Manfred Deng <manfreddeng@gmail.com>
2016-08-15 11:19:29 +1000

    Added notes to how to use neural networks using SGD. Started with the initial setup for neural networks.

---
+++ neuralNetwork.py
@@ -0,0 +1,31 @@
+
+import random
+import numpy as np
+
+class Network(object):
+
+    def __init__(self, sizes):
+        self.num_layers = len(sizes)
+        self.sizes = sizes
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.weights = [np.random.randn(y, x)
+                        for x, y in zip(sizes[:-1], sizes[1:])]
+
+
+    def SGD(self, training_data, epochs, mini_batch_size, eta):
+        if test_data: n_test = len(test_data)
+        n = len(training_data)
+        for j in xrange(epochs):
+            random.shuffle(training_data)
+            mini_batches = [training_data[k:k+mini_batch_size] for k in xrange(0, n, mini_batch_size)]
+            for mini_batch in mini_batches:
+                self.update_mini_batch(mini_batch, eta)
+            print "Test {0} complete".format(j)
+
+    def update_mini_batch(self, mini_batch, eta):
+
+def sigmoid(z):
+    return 1.0/(1.0+np.exp(-z))
+
+def sigmoid_prime(z):
+    return sigmoid(z)*(1-sigmoid(z))
